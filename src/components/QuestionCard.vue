bd465c3bb612813c49303ac9ee307a6611f2db58
visualjerk <jb@visualjerk.de>
2022-07-21 20:13:58 +0200

    add skip

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import ActionButton from '@/components/ActionButton.vue'
+
 import { Card } from '@/store/boxes'
 import { ref, unref, computed, watch, onMounted } from 'vue'
 
 const inputEl = ref()
 const props = defineProps<{
   card: Card
+  showBack?: boolean
 }>()
 const emit = defineEmits<{
   (e: 'success'): void
+  (e: 'error'): void
+  (e: 'skip'): void
 }>()
 
 const answer = ref('')
@@ -27,6 +32,14 @@ function reset() {
   unref(inputEl).focus()
 }
 
+async function handleError() {
+  emit('error')
+}
+
+async function handleSkip() {
+  emit('skip')
+}
+
 defineExpose({
   reset,
 })
@@ -36,24 +49,63 @@ onMounted(reset)
 
 <template>
   <div
-    class="flex items-center flex-col p-8 pt-4 border rounded-2xl shadow-2xl"
+    class="grid gap-4 p-10 border rounded-2xl shadow-2xl text-center"
     :class="[
       hasCorrectAnswer
         ? 'border-green-600 bg-green-50 shadow-green-300'
         : 'border-slate-200 bg-white shadow-slate-300',
     ]"
   >
-    <h2 class="text-9xl text-slate-700">{{ card.front }}</h2>
-    <input
-      ref="inputEl"
-      class="text-7xl text-center font-medium text-slate-800 bg-slate-200 max-w-[10rem] p-2 mt-8 rounded-xl focus:outline-8"
-      :class="[
-        hasCorrectAnswer
-          ? 'focus:bg-green-300 focus:outline-green-500'
-          : 'focus:bg-purple-200 focus:outline-purple-500',
-      ]"
-      autofocus
-      v-model="answer"
-    />
+    <h2 class="text-6xl font-medium text-slate-700 mb-6">{{ card.front }}</h2>
+    <div class="mb-6">
+      <input
+        v-if="showBack"
+        ref="inputEl"
+        class="w-full text-6xl text-slate-700 text-center font-medium bg-slate-200 p-2 rounded-xl outline-none"
+        :value="card.back"
+        readonly
+      />
+      <input
+        v-else
+        ref="inputEl"
+        class="w-full text-6xl text-slate-700 text-center font-medium bg-slate-200 p-2 rounded-xl focus:outline-8"
+        :class="[
+          hasCorrectAnswer
+            ? 'focus:bg-green-300 focus:outline-green-500'
+            : 'focus:bg-purple-200 focus:outline-purple-500',
+        ]"
+        autofocus
+        v-model="answer"
+      />
+    </div>
+    <ActionButton v-if="showBack" primary size="large" @click="handleSkip">
+      Next Card
+      <svg style="width: 24px; height: 24px" viewBox="0 0 24 24" class="ml-2">
+        <path
+          fill="currentColor"
+          d="M14 16.94V12.94H5.08L5.05 10.93H14V6.94L19 11.94Z"
+        />
+      </svg>
+    </ActionButton>
+    <div v-else class="grid sm:grid-cols-2 gap-4">
+      <ActionButton size="large" @click="handleError">
+        <svg style="width: 24px; height: 24px" viewBox="0 0 24 24" class="mr-2">
+          <path
+            fill="currentColor"
+            d="M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,4.5C17,4.5 21.27,7.61 23,12C21.27,16.39 17,19.5 12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C15.76,17.5 19.17,15.36 20.82,12C19.17,8.64 15.76,6.5 12,6.5C8.24,6.5 4.83,8.64 3.18,12Z"
+          />
+        </svg>
+        Show Answer
+      </ActionButton>
+      <ActionButton primary size="large" @click="handleSkip">
+        Skip Card
+        <svg style="width: 24px; height: 24px" viewBox="0 0 24 24" class="ml-2">
+          <path
+            fill="currentColor"
+            d="M14 16.94V12.94H5.08L5.05 10.93H14V6.94L19 11.94Z"
+          />
+        </svg>
+      </ActionButton>
+    </div>
   </div>
 </template>
--- src/views/Box/BoxLearn.vue
+++ src/views/Box/BoxLearn.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import LinkButton from '@/components/LinkButton.vue'
-import ActionButton from '@/components/ActionButton.vue'
 import QuestionCard from '@/components/QuestionCard.vue'
 
+import { nextTick, ref } from 'vue'
 import { wait } from '@/utils/wait'
 import { Box } from '@/store/boxes'
 import { useSession } from '@/store/sessions'
@@ -11,17 +11,28 @@ const props = defineProps<{
   box: Box
 }>()
 
+const showBack = ref(false)
+const boxEl = ref()
 const { currentCard, addError, addSuccess, nextCard } = useSession(props.box)
 
 async function handleSuccess() {
   addSuccess()
   await wait(1000)
   nextCard()
+  await nextTick()
+  boxEl.value.reset()
 }
 
 function handleError() {
   addError()
+  showBack.value = true
+}
+
+async function handleSkip() {
+  showBack.value = false
   nextCard()
+  await nextTick()
+  boxEl.value.reset()
 }
 </script>
 
@@ -30,15 +41,31 @@ function handleError() {
     <div class="flex justify-between items-center gap-3 mb-8">
       <h1>{{ box.name }}</h1>
       <div class="flex gap-3">
-        <LinkButton :to="`/box/${box.id}`">Stop Learning</LinkButton>
+        <LinkButton :to="`/box/${box.id}`">
+          <svg
+            style="width: 24px; height: 24px"
+            viewBox="0 0 24 24"
+            class="mr-2"
+          >
+            <path
+              fill="currentColor"
+              d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4M9,9V15H15V9"
+            />
+          </svg>
+          Stop Learning
+        </LinkButton>
       </div>
     </div>
     <div>
       <div class="grid gap-5 mb-6">
-        <QuestionCard :card="currentCard" @success="handleSuccess" />
-        <ActionButton primary size="large" @click="handleError">
-          Don't know
-        </ActionButton>
+        <QuestionCard
+          ref="boxEl"
+          :card="currentCard"
+          :show-back="showBack"
+          @success="handleSuccess"
+          @error="handleError"
+          @skip="handleSkip"
+        />
       </div>
     </div>
   </div>
